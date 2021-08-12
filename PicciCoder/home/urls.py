bfe7d50146485549eaaf9b850e8d6deaf41c7ee7
Picciprogrammar <mohammadiftehkarhasan@gmail.com>
2021-08-12 22:40:05 +0600

    slove Problem

--- PicciCoder/home/urls.py
+++ PicciCoder/home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-   path('', views.home, name='home')
+    
+    path('', views.home, name='home'),
+    path('Contact', views.Contact, name='Contact')
 ]
--- PicciCoder/home/views.py
+++ PicciCoder/home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('this is moon')
+    return render(request, 'home/home.html')
+
+def Contact(request):
+    return render(request, 'home/contact.html')
